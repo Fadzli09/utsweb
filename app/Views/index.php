cc3db2b2c5dcc77380cb3bafa9867308a974b7b9
Fadzli09 <fadzliaziz0109@gmail.com>
2023-05-15 13:18:15 +0700

    utsweb

---
+++ app/Views/index.php
@@ -0,0 +1,139 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Fazli</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800|Playfair+Display:,300, 400, 700" rel="stylesheet">
+
+  <link rel="stylesheet" href="../Assets/css/bootstrap.css">
+  <link rel="stylesheet" href="../Assets/css/animate.css">
+  <link rel="stylesheet" href="../Assets/css/owl.carousel.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/mediaelement@4.2.7/build/mediaelementplayer.min.css">
+
+  <link rel="stylesheet" href="../Assets/fonts/ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="../Assets/fonts/fontawesome/css/font-awesome.min.css">
+
+
+  <!-- Theme Style -->
+  <link rel="stylesheet" href="../Assets/css/style.css">
+</head>
+
+<body>
+
+  <div class="site-wrap">
+
+    <a href="#" class="offcanvas-toggle js-offcanvas-toggle">Menu</a>
+    <div class="offcanvas_menu" id="offcanvas_menu">
+      <ul class="mb-5">
+        <li class="active"><a href="/">Bio</a></li>
+        <li><a href="portfolio">Portfolio</a></li>
+        <li><a href="resume">Resume</a></li>
+        <li><a href="contact">Contact</a></li>
+      </ul>
+
+      <ul class="social">
+        <li><a href="https://www.facebook.com/profile.php?id=100006207374663&mibextid=ZbWKwL" class="fa fa-facebook"></a></li>
+        <li><a href="https://github.com/Fadzli09" class="mr-2 mb-2 fa fa-github"></a></li>
+        <li><a href="https://instagram.com/fadzli_19?igshid=ZGUzMzM3NWJiOQ==" class="fa fa-instagram"></a></li>
+      </ul>
+
+      </br><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+      Copyright &copy;<script>
+        document.write(new Date().getFullYear());
+      </script> All rights reserved.</a>
+      <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+
+    </div>
+
+    <aside style="background-image: url(../Assets/img/fotofazli.jpeg);"></aside>
+    <main>
+      <center>
+        <h2 class="mb-0">Hallo</h2> <br>
+        <h2 class="mb-0">Fadzli Aziz Jabbar</h2>
+      </center>
+      <div class="mb-4">
+        <center>
+          <p class="lead mb-4">!!!!!!!!!!!!!!!!!!!!!!!!!!</p>
+        </center>
+        <center>
+          <p>I am a student at the Muhammadiyah University of Sukabumi
+            I am interested in UI/UX design and Web Design. My hobby is playing games,
+            especially mobile and console games, I also like watching anime and I often do playing futsal.
+        </center>
+        <center>
+          <p>
+        </center>
+      </div>
+
+      <center>
+        <p>
+          <a href="https://www.facebook.com/profile.php?id=100006207374663&mibextid=ZbWKwL" class="mr-2 mb-2 fa fa-facebook"><br>Facebook</a>
+          <a href="https://github.com/Fadzli09" class="mr-2 mb-2 fa fa-github"><br>Github</a>
+          <a href="https://instagram.com/fadzli_19?igshid=ZGUzMzM3NWJiOQ==" class="mr-2 mb-2 fa fa-instagram"><br>Instagram</a>
+        </p>
+      </center>
+      <!-- <h2 class="mb-4 mt-5">Services</h2>
+        <div class="row">
+          <div class="col-md-6 col-lg-6 mb-4">
+            <div class="media custom-media  d-block">
+              <div class="icon mr-3"><span class="ion-android-cart"></span></div>
+              <div class="media-body">
+                <h3 class="mt-0"><a href="#">eCommerce Websites</a></h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed obcaecati totam.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-6 mb-4">
+            <div class="media custom-media  d-block">
+              <div class="icon mr-3"><span class="ion-bonfire"></span></div>
+              <div class="media-body">
+                <h3 class="mt-0"><a href="#">Branding Identity </a></h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed obcaecati, totam.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-6 mb-4">
+            <div class="media custom-media  d-block">
+              <div class="icon mr-3"><span class="ion-videocamera"></span></div>
+              <div class="media-body">
+                <h3 class="mt-0"><a href="#">Video Editing</a></h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed obcaecati totam.</p>
+              </div>
+            </div>
+          </div>
+          
+          <div class="col-md-6 col-lg-6 mb-4">
+            <div class="media custom-media  d-block">
+              <div class="icon mr-3"><span class="ion-cube"></span></div>
+              <div class="media-body">
+                <h3 class="mt-0"><a href="#">Web Applications </a></h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed obcaecati, totam.</p>
+              </div>
+            </div>
+          </div> -->
+  </div>
+
+  </main>
+  </div>
+
+  <!-- loader -->
+  <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
+      <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
+      <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#f4b214" />
+    </svg></div>
+
+  <script src="../Assets/js/jquery-3.2.1.min.js"></script>
+  <script src="../Assets/js/popper.min.js"></script>
+  <script src="../Assets/js/bootstrap.min.js"></script>
+  <script src="../Assets/js/owl.carousel.min.js"></script>
+  <script src="../Assets/js/jquery.waypoints.min.js"></script>
+  <script src="../Assets/js/jquery.countdown.min.js"></script>
+  <script src="../Assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ app/Views/resume.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>fazli aziz</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800|Playfair+Display:,300, 400, 700" rel="stylesheet">
+
+  <link rel="stylesheet" href="../Assets/css/bootstrap.css">
+  <link rel="stylesheet" href="../Assets/css/animate.css">
+  <link rel="stylesheet" href="../Assets/css/owl.carousel.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/mediaelement@4.2.7/build/mediaelementplayer.min.css">
+
+  <link rel="stylesheet" href="../Assets/fonts/ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="../Assets/fonts/fontawesome/css/font-awesome.min.css">
+
+
+  <!-- Theme Style -->
+  <link rel="stylesheet" href="../Assets/css/style.css">
+</head>
+
+<body>
+
+  <div class="site-wrap">
+
+    <a href="#" class="offcanvas-toggle js-offcanvas-toggle">Menu</a>
+    <div class="offcanvas_menu" id="offcanvas_menu">
+      <ul class="mb-5">
+        <li><a href="/">Bio</a></li>
+        <li><a href="portfolio">Portfolio</a></li>
+        <li class="active"><a href="resume">Resume</a></li>
+        <li><a href="contact">Contact</a></li>
+      </ul>
+      <ul class="social">
+
+        <li><a href="https://www.facebook.com/profile.php?id=100006207374663&mibextid=ZbWKwL" class="fa fa-facebook"></a></li>
+        <li><a href="https://github.com/Fadzli09" class="mr-2 mb-2 fa fa-github"></a></li>
+        <li><a href="https://instagram.com/fadzli_19?igshid=ZGUzMzM3NWJiOQ==" class="fa fa-instagram"></a></li>
+      </ul>
+
+      </br><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+      Copyright &copy;<script>
+        document.write(new Date().getFullYear());
+      </script> All rights reserved.</a>
+      <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+
+    </div>
+
+    <aside style="background-image: url(../Assets/img/fotofazli.jpeg);"></aside>
+    <main>
+
+      <h1 class="mb-3">Fazli Aziz Jabbar</h1>
+      <p>Saya seorang yang berpengalaman sebagai Teknisi Engineering memiliki analisis troubleshooting yang baik,
+        dapat menggunakan mesin perkakas, mudah beradaptasi dengan lingkungan baru, berkomitmen dengan pekerjaan,rajin, disiplin
+      </p>
+      <div class="mb-3">
+        <ul class="resume-list">
+          <li>
+            <h3>Pendidikan </h3>
+
+            <p> 2017 - 2020 : SMA Negeri 1 Parungkuda <br>
+              2014 - 2017 : SMP Negeri 1 Parungkuda <br>
+              2008 - 2014 : SD Negeri Palasari </p>
+          </li>
+
+        </ul>
+      </div>
+
+    </main>
+  </div>
+
+  <!-- loader -->
+  <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
+      <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
+      <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#f4b214" />
+    </svg></div>
+
+  <script src="../Assets/js/jquery-3.2.1.min.js"></script>
+  <script src="../Assets/js/popper.min.js"></script>
+  <script src="../Assets/js/bootstrap.min.js"></script>
+  <script src="../Assets/js/owl.carousel.min.js"></script>
+  <script src="../Assets/js/jquery.waypoints.min.js"></script>
+  <script src="../Assets/js/jquery.countdown.min.js"></script>
+  <script src="../Assets/js/main.js"></script>
+
+</body>
+
+</html>
